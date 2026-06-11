b11864cb144ae5eb53866a3b6b56b6170c0018eb
a26110064-ctrl <a26110064@ceti.mx>
2026-06-11 06:11:42 -0600

    Update calculadora.c
    Se agregan multiplicacion y division

--- calculadora.c
+++ calculadora.c
@@ -4,3 +4,10 @@ int main() {
     printf("Calculadora en desarrollo\n");
     return 0;
 }
+int multiplicacion(int a, int b){
+    return a*b;
+}
+
+float division(float a, float b){
+    return a/b;
+}
