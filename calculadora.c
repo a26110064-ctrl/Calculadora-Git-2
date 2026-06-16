463879c21c330a6833c5ba5df799722a60ffa890
a26110064-ctrl <a26110064@ceti.mx>
2026-06-16 12:55:51 -0600

    Update calculadora.c
    git commit -m "Juan Pablo crea estructura inicial de calculadora"

--- calculadora.c
+++ calculadora.c
@@ -4,3 +4,29 @@ int main() {
     printf("Calculadora en desarrollo\n");
     return 0;
 }
+
+#include <stdio.h>
+
+int main()
+{
+    int opcion;
+    float num1, num2, resultado;
+
+    printf("===== CALCULADORA =====\n");
+    printf("1. Suma\n");
+    printf("2. Resta\n");
+    printf("3. Multiplicacion\n");
+    printf("4. Division\n");
+
+    printf("Seleccione una opcion: ");
+    scanf("%d",&opcion);
+
+    printf("Ingrese primer numero: ");
+    scanf("%f",&num1);
+
+    printf("Ingrese segundo numero: ");
+    scanf("%f",&num2);
+
+
+    return 0;
+}
